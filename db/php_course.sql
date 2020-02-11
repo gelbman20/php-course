3ab8881d8119a034b1395c64fcef10f09aeddeb7
gelbman20 <gelbman20@gmail.com>
2020-02-11 11:39:40 +0200

    add: db

---
+++ db/php_course.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 11 2020 г., 12:34
+-- Версия сервера: 5.7.23
+-- Версия PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `php_course`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `name` varchar(160) NOT NULL,
+  `password` varchar(160) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`) VALUES
+(1, 'gel@gmail.com', 'gel', '$2y$10$OuPM.7.qiqoNw7BI5hnXzOV6WCMKmlLpmnZQUmoF8gjJa2rRcnmMi'),
+(2, 'gelgel@gmail.com', 'gelgel', '$2y$10$y96xf4CcL.QFMESQXtQKMueu4iZsI.pNbuiQgvP9ELDzZWgJadF.C'),
+(3, 'gel20@gmail.com', 'Andrii Helever', '$2y$10$acv6KgYkkwpR3DNlNLffJemNgZDmDYTSGq7TdDVUDeh2yTjr4GRCC'),
+(4, 'asdasd@asdas.com', '654456', '$2y$10$RiaVEoA0UUUCFnEKZkneFeVLrf3cq90RIlr7VPX3BJQ0ptQ4uvjc6');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
