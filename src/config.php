9a5dc33433bf8ffd7303770831c088664586de86
gelbman20 <gelbman20@gmail.com>
2020-02-17 09:41:24 +0200

    add: config.php

---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  "database" => [
+    "host" => "localhost",
+    "database" => "module01",
+    "username" => "root",
+    "password" => "",
+    "charset" => "utf8",
+    "connection" => "mysql:host"
+  ]
+];
