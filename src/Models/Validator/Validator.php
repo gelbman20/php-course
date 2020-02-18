aaf8737fe1fc4e6ca7aada14de61a818fc593318
gelbman20 <gelbman20@gmail.com>
2020-02-18 10:34:33 +0200

    sys: Validation in progress

--- src/Models/Validator/Validator.php
+++ src/Models/Validator/Validator.php
@@ -10,15 +10,50 @@ class Validator {
     $this->data = $data;
   }
   
-  public function check() {
+  public function checkData() {
     foreach ($this->data as $key => $value) {
-      $this->data[$key]['is_valid'] = false;
+      if($value['value'] !== null) {
+        $this->data[$key]['is_valid'] = false;
+      }
       
       if ($value['value']) {
         $this->data[$key]['is_valid'] = true;
       }
     }
-    
-    var_dump($this->data);
+  }
+
+  public function createErrorMessages() {
+    foreach ($this->data as $key => $value) {
+
+      // Default Validation
+      if ( !$value['is_valid'] && isset($value['is_valid']) ) {
+        $this->data[$key]['error_message'] = "The input must be field.";
+      }
+
+      // Email Validation
+      if ( $value['is_valid'] && $value['type'] === 'email' ) {
+        if ( !filter_var($value['value'], FILTER_VALIDATE_EMAIL) ) {
+          $this->data[$key]['is_valid'] = false;
+          $this->data[$key]['error_message'] = "The email {$value['value']} not correct.";
+        }
+      }
+    }
+  }
+
+  public function getErrorMessage($input_name) {
+    $input_data = $this->data[$input_name];
+    return $input_data['is_valid'] ? false : $input_data['error_message'];
+  }
+
+  public function getAll() {
+    return $this->data;
+  }
+
+  public function getOne($input_name) {
+    return $this->data[$input_name];
+  }
+
+  public function getValidationStatus() {
+
   }
 }
